6cee9777c7f5568e3856b99c5d459653522ef505
Ashutosh Karve <ashutoshkarve007@gmail.com>
2022-10-13 10:15:55 +0530

    Oled Display and MPU6050

--- IOT/ThinkSpeak/RandomValue/RandomValue.ino
+++ IOT/ThinkSpeak/RandomValue/RandomValue.ino
@@ -37,8 +37,7 @@ void setup() {
   WiFi.mode(WIFI_STA);   
   
   ThingSpeak.begin(client);  // Initialize ThingSpeak
-
-  
+ 
 }
 
 void loop() {
@@ -53,23 +52,13 @@ void loop() {
       } 
       Serial.println("\nConnected.");
     }
-long r1 = random(100);
-    // Get a new temperature reading
+    
+    long r1 = random(100);
  
     Serial.print("Temperature (ºC): ");
     Serial.println(r1);
-    
-    //uncomment if you want to get temperature in Fahrenheit
-    /*temperatureF = 1.8 * bme.readTemperature() + 32;
-    Serial.print("Temperature (ºC): ");
-    Serial.println(temperatureF);*/
-    
-    
-    // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
-    // pieces of information in a channel.  Here, we write to field 1.
+        
     int x = ThingSpeak.writeField(myChannelNumber, 1, r1, myWriteAPIKey);
-    //uncomment if you want to get temperature in Fahrenheit
-    //int x = ThingSpeak.writeField(myChannelNumber, 1, temperatureF, myWriteAPIKey);
 
     if(x == 200){
       Serial.println("Channel update successful.");
