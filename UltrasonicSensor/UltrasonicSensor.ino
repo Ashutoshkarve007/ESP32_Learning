77886d623173cc52b01be4ffbf3d153770579f76
Ashutosh Karve <ashutoshkarve007@gmail.com>
2022-11-09 11:40:25 +0530

    Update UltrasonicSensor.ino

--- UltrasonicSensor/UltrasonicSensor.ino
+++ UltrasonicSensor/UltrasonicSensor.ino
@@ -6,26 +6,26 @@ int distance;
 
 void setup() {
 
-pinMode(trigger_pin, OUTPUT); // configure the trigger_pin(D9) as an Output
-pinMode(Echo_pin, INPUT); // configure the Echo_pin(D11) as an Input
-Serial.begin(9600); // Enable the serial with 9600 baud rate
+  pinMode(trigger_pin, OUTPUT); // configure the trigger_pin(D9) as an Output
+  pinMode(Echo_pin, INPUT);     // configure the Echo_pin(D11) as an Input
+  Serial.begin(9600);           // Enable the serial with 9600 baud rate
 
 }
 
 void loop() {
-  
-digitalWrite(trigger_pin, LOW); //set trigger signal low for 2us
-delayMicroseconds(2);
 
-digitalWrite(trigger_pin, HIGH);  // make trigger pin active high
-delayMicroseconds(10);            // wait for 10 microseconds
-digitalWrite(trigger_pin, LOW);   // make trigger pin active low
+  digitalWrite(trigger_pin, LOW); //set trigger signal low for 2us
+  delayMicroseconds(2);
 
-duration = pulseIn(Echo_pin, HIGH); // save time duration value in "duration variable
-distance= duration*0.034/2; //Convert pulse duration into distance
+  digitalWrite(trigger_pin, HIGH);  // make trigger pin active high
+  delayMicroseconds(10);            // wait for 10 microseconds
+  digitalWrite(trigger_pin, LOW);   // make trigger pin active low
 
-Serial.print("Distance: ");
-Serial.print(distance);
-Serial.println(" cm");
-delay(1000);
+  duration = pulseIn(Echo_pin, HIGH); // save time duration value in "duration variable
+  distance = duration * 0.034 / 2;    //Convert pulse duration into distance
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(1000);
 }
